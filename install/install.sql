4a84f90a0747a79367ec60b3d3a5f7a2034b1f4e
T3chnoMaciej <maciejmotlawski@protonmail.com>
2016-08-05 17:30:20 +0200

    Update install.sql
    Deleted two unnecessery lines.

--- install/install.sql
+++ install/install.sql
@@ -4,6 +4,4 @@ CREATE TABLE IF NOT EXISTS `website`.`users` (
   `username` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'username needs no comment.',
   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'password in SHA256.',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_name` (`user_name`),
-  UNIQUE KEY `user_email` (`user_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='All users data.';
