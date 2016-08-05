38a4b30740cb93629ba163b7b186468054bf9ed8
T3chnoMaciej <maciejmotlawski@protonmail.com>
2016-08-05 17:32:37 +0200

    Update install.sql
    Syntax deleted

--- install/install.sql
+++ install/install.sql
@@ -2,6 +2,6 @@ CREATE DATABASE IF NOT EXISTS `website`;
 CREATE TABLE IF NOT EXISTS `website`.`users` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique user id, if somebody has same nickname.',
   `username` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'username needs no comment.',
-  `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'password in SHA256.',
-  PRIMARY KEY (`id`),
+  `password` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'password in SHA256.',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='All users data.';
